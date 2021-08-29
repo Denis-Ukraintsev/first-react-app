39f05d943087d36772fc63a9dcbe61fd86425eff
Denis Ukraintsev <den.from.oz@gmail.com>
2021-08-29 18:25:23 +0300

    Replace literals into separate objects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,37 @@
-import React from 'react';
+import React from 'react'
 
 const Navbar = () => {
+  const i18n = {
+    profile: 'Profile',
+    messages: 'Messages',
+    news: 'News',
+    music: 'Music',
+    settings: 'Settings',
+  }
+
   return (
-    <nav className='nav'>
+    <nav className="nav">
       <div>
-        <a >Profile </a>
+        <a>{i18n.profile}</a>
       </div>
       <br />
       <div>
-        <a> Messages </a>
+        <a>{i18n.messages}</a>
       </div>
       <br />
       <div>
-        <a> News </a>
+        <a>{i18n.news}</a>
       </div>
       <br />
       <div>
-        <a> Music </a>
+        <a>{i18n.music}</a>
       </div>
       <br />
       <div>
-        <a> Settings </a>
+        <a>{i18n.settings}</a>
       </div>
     </nav>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,25 +1,24 @@
-import React from 'react';
+import React from 'react'
 
 const Profile = () => {
+  const i18n = {
+    description: 'ava + description',
+    myPosts: 'My posts',
+    post1: 'Post1',
+    post2: 'Post2',
+  }
+
   return (
-    <div className='content'>
-    <div>
-      <img src='https://images4.alphacoders.com/697/697728.jpg'/>
+    <div className="content">
+      <div>
+        <img src="https://images4.alphacoders.com/697/697728.jpg" />
+      </div>
+      <div>{i18n.description}</div>
+      <div>{i18n.myPosts}</div>
+      <div>{i18n.post1}</div>
+      <div>{i18n.post2}</div>
     </div>
-    <div>
-      ava + description
-    </div>
-    <div>
-      My posts
-    </div>
-    <div>
-      post 1
-    </div>
-    <div>
-      post 2
-    </div>
-  </div>
-  );
+  )
 }
 
-export default Profile;
+export default Profile
