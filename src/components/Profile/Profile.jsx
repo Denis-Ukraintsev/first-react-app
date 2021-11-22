712b83a7c4f0d3442bb10b01e782209cb2828612
Denis Ukraintsev <den.from.oz@gmail.com>
2021-11-22 08:25:17 +0300

    Change Grid to Flex

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import MyPostsContainer from '..//Profile/MyPosts/MyPostsContainer'
-import ProfileInfo from './ProfileInfo/ProfileInfo'
 import styled from 'styled-components'
 
 const Profile = () => {
@@ -9,17 +8,26 @@ const Profile = () => {
   }
   return (
     <Root>
-      <ProfileInfo />
+      <Img img src="https://images4.alphacoders.com/697/697728.jpg" />
       <ItemContainer>{i18n.description}</ItemContainer>
       <MyPostsContainer />
     </Root>
   )
 }
 
-const Root = styled.div``
+const Root = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 5px;
+`
 const ItemContainer = styled.div`
   color: rgb(23, 88, 104);
   padding: 5px;
+`
+const Img = styled.img`
+  width: 1110px;
+  height: 500px;
+  border-radius: 5px;
 `
 
 export default Profile
